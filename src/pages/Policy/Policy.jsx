a261ce49ad947277a0c1c7c53d5109c4fd700f24
HaiderAli-32404 <32404@students.riphah.edu.pk>
2024-12-06 10:35:25 +0500

    Update Policy.jsx

--- src/pages/Policy/Policy.jsx
+++ src/pages/Policy/Policy.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './Policy.css';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
 const Policy = () => {
   return (
-    <div className="policy">
-      <h1>Our Policy</h1>
-      <p>Read our privacy and terms of service policy here.</p>
-      {/* Add the policy content */}
-    </div>
+    <>
+      <Header />
+      <div className="policy">
+        <h1>Our Policy</h1>
+        <p>Read our privacy and terms of service policy here.</p>
+        {/* Add the policy content */}
+      </div>
+      <Footer />
+    </>
   );
 };
 
 export default Policy;
+
