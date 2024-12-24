c1c871fd54dfdd3b734248af54ba88e3184971c7
HaiderAli-32404 <32404@students.riphah.edu.pk>
2024-12-24 18:06:00 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Import Pages
 import HomePage from './pages/HomePage/HomePage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import SignupPage from './pages/SignupPage/SignupPage';
 import ContractorLogin from './pages/ContractorLogin/ContractorLogin';
 import ContractorSignup from './pages/ContractorSignup/ContractorSignup';
-
-import BlogPage from './pages/BlogPage/BlogPage'; // Import the BlogPage
+import BlogPage from './pages/BlogPage/BlogPage';
 import UserDashboard from './pages/UserDashboard/UserDashboard';
 import ContractorDashboard from './pages/ContractorDashboard/ContractorDashboard';
 import NewsPage from './pages/NewsPage/NewsPage';
@@ -24,49 +23,54 @@ import Contact from './components/Contact/Contact';
 import GenerateFrontElevation from './pages/GenerateFrontElevation/GenerateFrontElevation';
 import CostCalculator from './pages/CostCalculator/CostCalculator';
 
+// ** Import the FindContractor component **
+import FindContractor from './pages/FindContractor/findContractor';
 
+// Import ContractorDetail for route
+import ContractorDetail from './pages/ContractorDetail/ContractorDetail';
+
+// FontAwesome CSS
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
-
-
-
-
-
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/contractor-login" element={<ContractorLogin />} />
         <Route path="/contractor-signup" element={<ContractorSignup />} />
-        <Route path="/userdashboard" element={<UserDashboard />} />
-        <Route path="/ContractorDashboard" element={<ContractorDashboard />} />
-
-        
-        {/* Add the Blogs route */}
-        <Route path="/news" element={<NewsPage />} /> {/* Route for NewsPage */}
-        <Route path="/news/ai-transforming-construction" element={<AITransformingConstruction />} />
-        <Route path="/news/latest-trends" element={<LatestTrends />} />
-        <Route path="/news/future-construction-tech" element={<FutureConstructionTech />} />
-        <Route path="/blogs" element={<BlogPage />} />
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/policy" element={<Policy />} />
         <Route path="/help-support" element={<HelpSupport />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/" element={<BookConsultation />} />
+
+        {/* Features */}
         <Route path="/generate-front-elevation" element={<GenerateFrontElevation />} />
         <Route path="/cost-calculation" element={<CostCalculator />} />
-        
+        <Route path="/book-consultation" element={<BookConsultation />} />
 
-      
+        {/* Contractor Pages */}
+        <Route path="/contractors" element={<FindContractor />} />
+        <Route path="/contractor/:id" element={<ContractorDetail />} />
 
+        {/* Blogs and News */}
+        <Route path="/blogs" element={<BlogPage />} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/news/ai-transforming-construction" element={<AITransformingConstruction />} />
+        <Route path="/news/latest-trends" element={<LatestTrends />} />
+        <Route path="/news/future-construction-tech" element={<FutureConstructionTech />} />
+
+        {/* Dashboards */}
+        <Route path="/userdashboard" element={<UserDashboard />} />
+        <Route path="/contractordashboard" element={<ContractorDashboard />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
