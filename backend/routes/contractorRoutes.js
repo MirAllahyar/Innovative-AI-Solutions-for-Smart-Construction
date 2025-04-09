b78cfabcb6a64f79cd992e3bbb5b44089c12a260
Syed Touqeer Hussain shah <140242132+tuqeer6@users.noreply.github.com>
2025-04-09 21:30:12 +0500

    Update contractorRoutes.js

--- backend/routes/contractorRoutes.js
+++ backend/routes/contractorRoutes.js
@@ -1,8 +1,24 @@
-const express = require('express');
-const { saveContractorData } = require('../controllers/contractorController');
+import express from "express";
+import {
+  getAllContractors,
+  updateContractorProfile,
+  getContractor,
+  getContractorById,
+  rateContractor,
+} from "../controller/contractor.controller.js";
+import { isAuthenticated } from "../middleware/isAuthenticated.js";
 const router = express.Router();
 
-// POST route to save contractor data
-router.post('/save', saveContractorData);
+router.get("/getAllContractors", getAllContractors);
 
-module.exports = router;
+router.patch(
+  "/update-contractor-profile",
+  isAuthenticated,
+  updateContractorProfile
+);
+router.post("/rate/:id", isAuthenticated, rateContractor);
+
+router.get("/get-contractor", isAuthenticated, getContractor);
+router.get("/get-contractor/:id", getContractorById);
+
+export default router;
