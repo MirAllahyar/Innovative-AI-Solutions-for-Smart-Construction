77f3f864916550d60def9f4630d1b1b416211be1
MirAllahyar <165079751+MirAllahyar@users.noreply.github.com>
2025-04-20 13:42:46 +0500

    Update UserContext.js

---
+++ src/context/UserContext.js
@@ -0,0 +1,29 @@
+import React, { createContext, useState, useContext, useEffect } from "react";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(() => {
+    const storedData = localStorage.getItem("user");
+
+    return storedData ? JSON.parse(storedData) : null;
+  });
+
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+  }, [user]);
+  const logout = () => {
+    setUser(null);
+  };
+  return (
+    <UserContext.Provider value={{ user, setUser, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContext);
